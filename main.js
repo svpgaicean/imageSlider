e9c06a73a17bb6a5b0d79fe6127c6068180a3a66
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-09-03 13:53:54 +0300

    Fix last->first / first->last transition glitch

--- main.js
+++ main.js
@@ -2,74 +2,90 @@
 const thumbBar = document.querySelector('.thumb-bar');
 const dotBar = document.querySelector('.dot-bar');
 const slider = document.querySelector('.slider');
+
+let size; // image width
+let initSize;
+
+let count = 10; // hardcoded for now
+function parseImages(count) {
+	for (let i = 1; i <= count; i++) {
+		const newThumbImg = document.createElement('img');
+		const newSliderImg = document.createElement('img');
+		const newDot = document.createElement('span');
+
+		newThumbImg.setAttribute('src', `./images/pic${i}.jpg`)
+		newThumbImg.setAttribute('class', `idx${i}`);
+		newSliderImg.setAttribute('src', `./images/pic${i}.jpg`)
+		if (i === 1) {
+			newDot.setAttribute('class', `dot idx${i} active`);
+		} else {
+			newDot.setAttribute('class', `dot idx${i}`);
+		}
+		
+		thumbBar.appendChild(newThumbImg);
+		newThumbImg.addEventListener('click', renderImg);
+		dotBar.appendChild(newDot);
+		newDot.addEventListener('click', renderImg);
+
+		slider.appendChild(newSliderImg);
+	}
+}
+parseImages(10);
+
 const images = document.querySelectorAll('.slider img');
 
-// const transitionScheme = 'transform 1.5s ease-in-out';
+images[0].addEventListener('load', () => {
+	size = images[0].clientWidth;
+	initSize = -(size + size/2);
+	slider.prepend(slider.lastElementChild);
+	slider.style.transform = `translateX(${initSize}px)`;
+});
+
 const transitionScheme = 'transform 0.5s ease-in-out';
 
 // Buttons
-var nextBtn = document.querySelector('.nextBtn');
-var prevBtn = document.querySelector('.prevBtn');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
 
 // Counter
-let counter = 1;
-const initSize = images[0].clientWidth/2;
-const size = images[0].clientWidth;
-// initial positioning of main display image
-slider.style.transform = 'translateX(' + (-initSize * counter) + 'px)';
+let counter = 0;
+let amount;
+let direction;
 
 // Button Listeners
 nextBtn.addEventListener('click', () => {
-	if (counter >= images.length - 1) return;	
+	direction = -1;
 	slider.style.transition = transitionScheme; 
 	counter++;
-	slider.style.transform = 'translateX(' + ((-size * counter)+initSize) + 'px)';
+	amount = direction * size + initSize;
+	slider.style.transform = `translateX(${amount}px)`;
 });
 
 prevBtn.addEventListener('click', () => {
-	if (counter <= 0) return; 
+	direction = 1;
 	slider.style.transition = transitionScheme; 
 	counter--;
-	slider.style.transform = 'translateX(' + ((-size * counter)+initSize) + 'px)';
+	amount = direction * size + initSize;
+	slider.style.transform = `translateX(${amount}px)`;
 });
 
 slider.addEventListener('transitionend', () => {
-	if (images[counter].id === 'last') {
-		slider.style.transition = 'none';
-		counter = images.length - 2; // minus the first and last clone
-		slider.style.transform = 'translateX(' + ((-size * counter)+initSize) + 'px)';
+	if (direction === 1) {
+		slider.prepend(slider.lastElementChild);
+	} else if (direction === -1) {
+		slider.appendChild(slider.firstElementChild);
 	}
-	if (images[counter].id === 'first') {
-		slider.style.transition = 'none';
-		counter = images.length - counter; // plus the first and last clone
-		slider.style.transform = 'translateX(' + ((-size * counter)+initSize) + 'px)';
-	}
-})
 
-/* Looping through images */
-for (let i = 1; i <= 5; i++) {
-	// thumbBar elements
-	const newImage = document.createElement('img');
-	newImage.setAttribute('src', `./images/pic${i}.jpg`);
-	newImage.setAttribute('class', `idx${i}`);
-	thumbBar.appendChild(newImage);
-	newImage.addEventListener('click', renderImg);
-
-	// dots elements
-	const newDot = document.createElement('span');
-	if (i === 1) {
-		newDot.setAttribute('class', `dot idx${i} active`);
-	} else {
-		newDot.setAttribute('class', `dot idx${i}`);
-	}
-	dotBar.appendChild(newDot);
-	newDot.addEventListener('click', renderImg);
-}
+	slider.style.transition = 'none';
+	slider.style.transform = `translate(${initSize}px)`;
+	setTimeout( () => {
+		slider.style.transition = transitionScheme;
+	})
+});
 
 function renderImg(e) {
 	let src = e.target.getAttribute('class');
 	let pos = src.charAt(src.search(/[0-9]+/));
-	// console.log(`pos-counter = ${pos-counter} .. ${Math.floor(5/2)}`);
 	if (pos > counter) {
 		while (counter < pos) {
 			nextBtn.click();
@@ -90,29 +106,13 @@ function toggleActiveDot() {
 
 	activeDot[0].className = activeDot[0].className.replace(" active", "");
 
-	if (counter > 5) {
+	if (counter > 10) {
 		displayIdx = 1;
 	}
 	else if (counter < 1) {
-		displayIdx = 5;
+		displayIdx = 10;
 	}
 	dots[displayIdx-1].className += " active";
 }
 
 slider.addEventListener('transitionstart', toggleActiveDot);
-
-// /* Wiring up the Darken/Lighten button */
-// function changeBrightness() {
-// 	let btn_class = btn.getAttribute('class');
-
-// 	if (btn_class === 'dark') {
-// 		btn.setAttribute('class', 'light');
-// 		btn.textContent = 'Lighten';
-// 		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-// 	} else {
-// 		btn.setAttribute('class', 'dark');
-// 		btn.textContent = 'Darken';
-// 		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-// 	}
-// }
-// btn.addEventListener('click', changeBrightness);
