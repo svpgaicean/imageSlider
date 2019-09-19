510a8c070dc5c2d440d40151f225b4bf1bd64d88
Patrik Gaicean <patrik.gaicean@softvision.ro>
2019-09-19 15:36:12 +0300

    Rename most variables and functions

--- main.js
+++ main.js
@@ -12,23 +12,13 @@ const prevBtn = document.querySelector('.prevBtn');
 const nextThumb = document.querySelector('.nextThumb');
 const prevThumb = document.querySelector('.prevThumb');
 
-let size = 640;
-// let size; // image width; TOFIX: displacement will vary each image size
-// required for initial positioning of slider
-let initSize; /** TODO: give a decent name -- initDisplacement ? */
-
-const transitionScheme = 'transform 0.5s ease-in-out';
-
 // Local Variables 
-let counter = 0; // current position on slider
-/** number of images to transition over 
- *  used to calculate individual size of all images used in translation
- */
-let moveAmount = 0;
-// direction no longer used, moving everything based on displacement on X
-// let direction; // movement direction based on next/prev buttons
-let imageCount; // total number of images
-let thumbCount = 0; // counter for thumbBar images
+let totalImages = 0;
+let sliderPosition = 0;
+let thumbBarPosition = 0;
+let initialDisplacement = 0;
+let currentDisplacement = 640;
+const transitionScheme = 'transform 0.5s ease-in-out';
 
 function init() {
 	getJSON("http://localhost:5000/images")
@@ -36,15 +26,15 @@ function init() {
 		.catch(function (err) {
 			console.log(err);
 		})
-    .then(generateElements);
+    .then(generateElements)
 		// .then(autoSlide);
 }
 
 function getJSON(url) {
 	return new Promise(function(resolve, reject) {
-		const xhr = new XMLHttpRequest;
-
-		xhr.onload = function() { // 404 goes here aswell
+    const xhr = new XMLHttpRequest;
+    
+    xhr.onload = function() { // 404 goes here aswell
 			resolve(this);
 		};
 
@@ -74,7 +64,7 @@ function parseJSON(xhr) {
 
 function moveSlider() {
   slider.style.transition = transitionScheme;
-  let amount = (counter * -size) + initSize;
+  let amount = -(sliderPosition * currentDisplacement) + initialDisplacement;
   slider.style.transform = `translateX(${amount}px)`;
 }
 
@@ -84,46 +74,15 @@ function moveThumb() {
    *  ii) handle case where active image is not on thumbar current view
    */
   thumbBar.style.transition = transitionScheme;
-  let amount = -(thumbCount * (640/4));
+  let amount = -(thumbBarPosition * (640/4));
   thumbBar.style.transform = `translateX(${amount}px)`;
 }
 
-function getDisplayIndex(counter, imgCount) {
-  let displayIdx;
-
-	if (counter >= 0) {
-		displayIdx = counter % imgCount;
-	} else if (counter < 0) {
-		displayIdx = imgCount - (Math.abs(counter) % imgCount);
-		if (displayIdx === imgCount) displayIdx = 0;
-  }
-
-  return displayIdx;
-}
-
 function renderImg(e) {
-  if (e) {
-    const currentImage = getDisplayIndex(counter, imageCount);
-    const chosenImage = whichChild(e.target);
-    const delta = chosenImage - currentImage;
-    counter += delta;
-    // console.log(counter);
-
-    // console.log(
-    //   `render: chosenImg ${chosenImage},
-    //   counter ${counter},
-    //   currentImg ${currentImage}`
-    //  );
-
-    moveAmount = chosenImage - currentImage;
-    if (moveAmount < 0) {
-      // direction = 1;
-    } else if (moveAmount > 0) {
-      // direction = -1;
-    }
-    // moveAmount = Math.abs(moveAmount);
-    // console.log(`moveAmount: ${moveAmount}`);
-  }
+  const currentImage = sliderPosition;
+  const chosenImage = whichChild(e.target);
+  const delta = chosenImage - currentImage;
+  sliderPosition += delta;
 
   moveSlider();
 }
@@ -156,106 +115,88 @@ function autoSlide() {
 }
 
 function generateElements(jsonData) {
-	imageCount = jsonData.data.length;
-  // const sliderImages = generateSlider(jsonData, imageCount);
-  generateSlider(jsonData, imageCount);
+	totalImages = jsonData.data.length;
+  generateSlider(jsonData);
   addSliderExtremities();
   const sliderImages = document.querySelectorAll('.slider img');
 	alignSliderImages(sliderImages[0]);
-  generateThumbBar(jsonData, imageCount);
+  generateThumbBar(jsonData);
 
 	container.insertAdjacentHTML('beforeend', '<p class="image-current"></p>');
 	container.insertAdjacentHTML('beforeend', '<p class="image-name"></p>');
   container.insertAdjacentHTML('beforeend', '<p class="image-description"></p>');
   
-  toggleActive(counter, imageCount);
+  toggleActive();
   
 	slider.addEventListener('transitionend', () => {
-		toggleActive(counter, imageCount);
+		toggleActive();
   });	
 }
 
 function addSliderExtremities() {
- // clone sliderImages 0 and 1
- // clone sliderImages length and length-1
- const children = [...slider.children];
- const len = children.length;
- const clone0 = children[0].cloneNode(true)
- const clone1 = children[1].cloneNode(true)
- const cloneN = children[len-1].cloneNode(true)
- const cloneN1 = children[len-2].cloneNode(true)
- clone0.setAttribute('class', 'cloneZERO');
- slider.appendChild(clone0);
- slider.appendChild(clone1);
- slider.appendChild(cloneN1);
- slider.appendChild(cloneN);
- slider.prepend(slider.lastElementChild);
- slider.prepend(slider.lastElementChild);
+  const children = [...slider.children];
+  const len = children.length;
+  const clone0 = children[0].cloneNode(true);
+  const clone1 = children[1].cloneNode(true);
+  const cloneN = children[len-1].cloneNode(true);
+  const cloneN1 = children[len-2].cloneNode(true);
+
+  slider.appendChild(clone0);
+  slider.appendChild(clone1);
+  slider.appendChild(cloneN1);
+  slider.appendChild(cloneN);
+  slider.prepend(slider.lastElementChild);
+  slider.prepend(slider.lastElementChild);
 }
 
 function alignSliderImages(firstImage) {
 	firstImage.addEventListener('load', () => {
     const images = document.querySelectorAll('.slider img');
-    // console.log(images);
-    const one = images[0].clientWidth;
-    const two = images[1].clientWidth;
-    const three = images[2].clientWidth;
-    // console.log(`window: ${window.innerWidth}`);
-    // console.log(`
-    //   sizeC1 ${one}
-    //   sizeC ${two}
-    //   sizeDisp ${three}
-    // `);
-    initSize = -(one + two + three/2) + 1280/2;
+    const firstImgWidth = images[0].clientWidth;
+    const secondImgWidth = images[1].clientWidth;
+    const mainImgWidth = images[2].clientWidth;
+    const mainImgCentered = mainImgWidth / 2;
+
+    initialDisplacement = -(firstImgWidth + secondImgWidth + mainImgCentered) + 1280 / 2;
     // TODO: for fullscreen version use window.innerWidth instead of 1280px
-    slider.style.transform = `translateX(${initSize}px)`;
+    slider.style.transform = `translateX(${initialDisplacement}px)`;
 	});
 }
 
-function generateSlider(jsonData, count) {
-	if (count === 0) {
-		// handle no image
-	}	else if (count === 1) {
-		// handle one image
-	}	else {
-		for (let i = 0; i < count; i++) {
-      const sliderDiv = document.createElement('div');
-      const sliderImg = document.createElement('img');
-      const sliderImgName = document.createElement('p');
-      const sliderImgDescr = document.createElement('p');
-      const displayIdx = getDisplayIndex(i, count);
+function generateSlider(jsonData) {
+  let count = totalImages;
+  for (let i = 0; i < count; i++) {
+    const sliderDiv = document.createElement('div');
+    const sliderImg = document.createElement('img');
+    const sliderImgName = document.createElement('p');
+    const sliderImgDescr = document.createElement('p');
 
-      if (i === 0) {
-        sliderDiv.setAttribute('class', ' activeS');
-      }
-			sliderImg.setAttribute('src', `${jsonData.data[i].url}`);
-      sliderImg.setAttribute('alt', `${jsonData.data[i].name}`);
-      sliderImgName.setAttribute('class', 'img-name');
-      sliderImgDescr.setAttribute('class', 'img-description');
-      sliderImgName.textContent = `${jsonData.data[displayIdx].name}`;
-      sliderImgDescr.textContent = `${jsonData.data[displayIdx].short_description}`;
-      /** TODO: make text disappear if it 'overflows'
-       *  only show text on current active image
-       */
-			sliderDiv.appendChild(sliderImg);
-			sliderDiv.appendChild(sliderImgName);
-      sliderDiv.appendChild(sliderImgDescr);
-      slider.appendChild(sliderDiv);
-		}
-
-		// append clone elements to slider for correct behaviour 
-		const children = [...slider.children];
-		while (count < 5) {
-			const clones = children.map(child => child.cloneNode(true));
-			clones.forEach(clone => slider.appendChild(clone));
-			count += clones.length;
-		}
+    sliderImg.setAttribute('src', `${jsonData.data[i].url}`);
+    sliderImg.setAttribute('alt', `${jsonData.data[i].name}`);
+    sliderImgName.setAttribute('class', 'img-name');
+    sliderImgDescr.setAttribute('class', 'img-description');
+    sliderImgName.textContent = `${jsonData.data[i].name}`;
+    sliderImgDescr.textContent = `${jsonData.data[i].short_description}`;
+    /** TODO: make text disappear if it 'overflows'
+     *  only show text on current active image
+     */
+    sliderDiv.appendChild(sliderImg);
+    sliderDiv.appendChild(sliderImgName);
+    sliderDiv.appendChild(sliderImgDescr);
+    slider.appendChild(sliderDiv);
   }
-  
-	// return document.querySelectorAll('.slider img'); 
+  /** TOFIX: was working for old slider implementation */
+  // append clone elements to slider for correct behaviour 
+  const children = [...slider.children];
+  while (count < 5) {
+    const clones = children.map(child => child.cloneNode(true));
+    clones.forEach(clone => slider.appendChild(clone));
+    count += clones.length;
+  }
 }
 
-function generateThumbBar(jsonData, count) {
+function generateThumbBar(jsonData) {
+  let count = totalImages;
 	for (let i = 0; i < count; i++) {
 		const newThumbImg = document.createElement('img');
 		newThumbImg.setAttribute('src', `${jsonData.data[i].url}`);
@@ -268,63 +209,76 @@ function generateThumbBar(jsonData, count) {
 	}
 }
 	
-function toggleActive(counter, imgCount) {
+function toggleActive() {
 	const imgCurrent = document.querySelector('.image-current');
   const images = document.querySelectorAll('.thumb-bar img');
-  const displayIdx = getDisplayIndex(counter, imgCount);
   const activeImage = document.getElementsByClassName("active");
 
 	activeImage[0].className = activeImage[0].className.replace(" active", "");
-  images[displayIdx].className += " active";
+  images[sliderPosition].className += " active";
 
-  imgCurrent.textContent = `Image ${displayIdx + 1} of ${imgCount}`;
+  imgCurrent.textContent = `Image ${sliderPosition + 1} of ${totalImages}`;
 }
 
 /** Event Listeners */
 // Buttons
 nextBtn.addEventListener('click', () => {
-  if (counter > imageCount) return;
-  counter += 1;
-  moveAmount += 1;
+  if (sliderPosition > totalImages) return;
+  sliderPosition += 1;
+  
+  if (sliderPosition >= 4 && sliderPosition < totalImages) {
+    thumbBarPosition += 1;
+    moveThumb();
+  } else if (sliderPosition === totalImages) {
+    thumbBarPosition = 0;
+    moveThumb();
+  };
 
   moveSlider();
 });
 
 prevBtn.addEventListener('click', () => {
-  if (counter < 0) return;
-  counter -= 1;
-  moveAmount -= 1;
-  
+  if (sliderPosition < 0) return;
+  sliderPosition -= 1;
+
+  if (sliderPosition >= 3) {
+    thumbBarPosition -= 1;
+    moveThumb();
+  } else if (sliderPosition < 0) {
+    thumbBarPosition = totalImages - 4;
+    moveThumb();
+  };
+
   moveSlider();
 });
 
 nextThumb.addEventListener('click', () => {
   // 4 images displayed at 1st, so max 6 movements, hardcoded for now
-  if (thumbCount === 6) return;
-  else thumbCount += 1;
+  if (thumbBarPosition === 6) return;
+  else thumbBarPosition += 1;
 
   moveThumb();
-})
+});
 
 prevThumb.addEventListener('click', () => {
-  if (thumbCount === 0) return;
-  else thumbCount -= 1;
+  if (thumbBarPosition === 0) return;
+  else thumbBarPosition -= 1;
 
   moveThumb();
-})
+});
 
 // Slider
 slider.addEventListener('transitionend', function() {
   let amount = 0;
-  if (counter < 0) {
+  if (sliderPosition < 0) {
     slider.style.transition = 'none';
-    counter = imageCount - 1;
-    amount = initSize - size * counter;
+    sliderPosition = totalImages - 1;
+    amount = initialDisplacement - currentDisplacement * sliderPosition;
     slider.style.transform = `translateX(${amount}px)`;
-  } else if (counter >= imageCount) {
+  } else if (sliderPosition >= totalImages) {
     slider.style.transition = 'none';
-    counter = 0;
-    amount = initSize;
+    sliderPosition = 0;
+    amount = initialDisplacement;
     slider.style.transform = `translateX(${amount}px)`;
   }
 });
